7a4898400a6fdeef3d9f4d9848ad21f3ec798173
Glitch (v12-kayit-altyapi) <none>
2021-06-25 05:22:12 +0000

    🐾⚾️ Checkpoint ./komutlar/bilgi/yardım.js:33042553/14275 ./ayarlar.json:33042553/107

--- komutlar/bilgi/yardım.js
+++ komutlar/bilgi/yardım.js
@@ -1,33 +1,33 @@
 const Discord = require('discord.js');
-const ayarlar = require('../../ayarlar.json');
-
-let botid = ('856882009451921468') //bu yere botun id'sini yapıştırın.
-//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
-
-exports.run = (client, message, args) => {
-    const embed = new Discord.MessageEmbed()
-        .setAuthor(`${client.user.username} `, client.user.avatarURL())
-        .setColor('0x36393E')
-        .setTitle(`${client.user.username} - Komutlar`)
-        .setDescription(` | **${ayarlar.prefix}yetkili** Yetkili Komutları.\n  | **${ayarlar.prefix}kullanıcı** Kullanıcıya Komutları.\n  |  **${ayarlar.prefix}eğlence** Eğlence Komutları.\n  | **${ayarlar.prefix}oyun**  Oyun Komutları Gösterir.\n` + `| **${ayarlar.prefix}seviyeyardım**  Seviyeyardım Komutları Gösterir.\n`)  
-        .setThumbnail(client.user.avatarURL())
-        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=856882009451921468&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/aEUfsU9) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
-        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL())
-        .setImage("https://cdn.discordapp.com/attachments/607989228294635543/660761252825399297/asreaper_banenr.jpg")  
-    return message.channel.send(embed);
-  
-  
-};
+const dragon = require("../../ayarlar.json");
+const db = require('quick.db')
+exports.run = async (client, msg, args) => { 
+  let prefix = await db.fetch(`prefix.${msg.guild.id}`) || dragon.prefix 
+  let p = args[0];
+    
+const bot = new Discord.MessageEmbed()    
+.setAuthor(`${client.user.username} Yardım Menüsü`,client.user.avatarURL())
+.setColor('0x36393E')
+.setDescription(`<:redhypesquad:849726093488291892> Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
+.addField(`> <:reduser:849726099105906688> __Genel Komutlar__ » \`${prefix}genel\` `,`➥ ***Genel Komutları Gösterir***`)
+.addField(`> <:redguard:849738272200458271> __Koruma Komutlar__ » \`${prefix}koruma\` `,`➥ ***Koruma Komutlarını Gösterir***`)
+.addField(`> <:redsettings:849726094810677309> __Sunucu Komutlar__ » \`${prefix}sunucu\` `,`➥ ***Sunucu Komutlarını Gösterir***`)
+.addField(`> <:redstar:849726094931263558> __Eğlence Komutlar__ » \`${prefix}eğlence\` `,`➥ ***Eğlence Komutlarını Gösterir***`)
+.addField(`> <:redgamepad:849726093370851328> __Oyun Komutlar__ » \`${prefix}oyunlar\` `,`➥ ***Oyun Komutlarını Gösterir***`)
+.addField(`> <:redplus:849726093660651570> __Eklenti Komutlar(EKLENECEK)__ » \`${prefix}eklenti\` `,`➥ ***Eklenti Komutlarını Gösterir***`)
+.addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=856882009451921468&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/aEUfsU9) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+.setThumbnail(client.user.avatarURL)
+.setImage(dragon.banner)
+msg.channel.send(bot)
+  };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['help'],
-  permLevel: 0,
-};
+  guildOnly: true,
+  aliases: ["help"],
+  permLevel: 0
+}
 
 exports.help = {
-  name: 'yardım',
-  description: '',
-  usage: ''
+  name: 'yardım'
 };
