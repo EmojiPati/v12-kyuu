40ce14ff8cb3ec46fdee9bb34dd846d029b02199
Glitch (v12-kayit-altyapi) <none>
2021-06-29 10:20:25 +0000

    🎯🛳 Checkpoint ./komutlar/bilgi/davet.js:33042553/77 ./komutlar/banaözel/eval.js:33042553/2018 ./package.json:33042553/2864

--- komutlar/bilgi/davet.js
+++ komutlar/bilgi/davet.js
@@ -4,10 +4,10 @@ exports.run = async (client, msg, args) => {
   
     const embed = new Discord.MessageEmbed()
         .setTitle(`${client.user.username} Davet `)
-        .setDescription(`<:redlink:849738273042858035> **Botun Davet Linki İçin** [TIKLA](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+        .setDescription(`<:blurplelink:857907168430391336> **Botun Davet Linki İçin** [TIKLA](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
         .setThumbnail(client.user.avatarURL)
         .setFooter(`${msg.author.username} Başarıyla Davet Sistemi Kullandı`, msg.author.avatarURL)
-    .setColor(`#ffffff`)
+    .setColor(`0x36393E`)
 msg.channel.send(embed)
 
 };
