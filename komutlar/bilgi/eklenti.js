b2cb11ce798c5905ddc638a8f247f006088def27
Glitch (short-inconclusive-printer) <none>
2021-06-29 17:05:21 +0000

    👸👩‍❤️‍💋‍👩 Checkpoint ./komutlar/bilgi/eklenti.js:33042553/61 ./komutlar/bilgi/seviye-sistemi.js:33042553/3453

--- komutlar/bilgi/eklenti.js
+++ komutlar/bilgi/eklenti.js
@@ -5,10 +5,10 @@ const prefix = dragon.prefix
 exports.run = async (client, msg, args) => { 
     
 const bot = new Discord.MessageEmbed()
-.setAuthor(`${client.user.username} Genel Yardım Menüsü`,client.user.avatarURL())
+.setAuthor(`${client.user.username} Eklenti Menüsü`,client.user.avatarURL())
 .setColor('0x36393E')
 .setDescription(`<:blurplehypesquad:857920353179009044> Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
-.addField(`> <:blurpletada:859401334523559956> __Çekiliş Sistemi__ » \`${prefix}çekiliş-sistemi\` `,`➥ ***Gelişmiş Çekiliş Sistemi***`)
+.addField(`> <:blurpletada:859401334523559956> __Çekiliş Sistemi(EKLENECEK)__ » \`${prefix}çekiliş-sistemi\` `,`➥ ***Gelişmiş Çekiliş Sistemi***`)
 .addField(`> <:blurplerocket:857907158565519360> __Seviye Sistemi__ » \`${prefix}seviye-sistemi\` `,`➥ ***Gelişmiş Seviye Sistemi***`)
 .addField(`» <:blurplelink:857907168430391336> Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
 .setThumbnail(client.user.avatarURL)
@@ -19,7 +19,7 @@ const bot = new Discord.MessageEmbed()
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["eklenti"],
+  aliases: ["addons"],
   permLevel: 0
 }
 
