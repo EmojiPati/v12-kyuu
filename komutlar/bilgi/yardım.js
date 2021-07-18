53f43ca8fba43a8ba4d3e1dbd0ef04a97aa1b27e
Glitch (short-inconclusive-printer) <none>
2021-07-18 20:06:37 +0000

    💘🎪 Checkpoint ./komutlar/bilgi/yardım.js:33042553/534 ./komutlar/bilgi/reaksiyon.js:33042553/1728 ./emoji.json:33042553/69

--- komutlar/bilgi/yardım.js
+++ komutlar/bilgi/yardım.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js');
 const dragon = require("../../ayarlar.json");
 const db = require('quick.db')
-const { user, hypequa }
+const { user, hypesquad, guard, settings, star, integrasyon, plus, link } = require('../../emoji.json')
 exports.run = async (client, msg, args) => { 
   let prefix = await db.fetch(`prefix.${msg.guild.id}`) || dragon.prefix 
     
 const bot = new Discord.MessageEmbed()    
 .setAuthor(`${client.user.username} Yardım Menüsü`,client.user.avatarURL())
 .setColor('0x36393E')
-.setDescription(`<:blurplehypesquad:857920353179009044> Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
-.addField(`> <:blurpleuser:857907167898239007> __Genel Komutlar__ » \`${prefix}genel\` `,`➥ ***Genel Komutları Gösterir***`)
-.addField(`> <:blurpleguard:857907154357977128> __Koruma Komutlar__ » \`${prefix}koruma\` `,`➥ ***Koruma Komutlarını Gösterir***`)
-.addField(`> <:blurplesettings:857907152315744278> __Sunucu Komutlar__ » \`${prefix}sunucu\` `,`➥ ***Sunucu Komutlarını Gösterir***`)
-.addField(`> <:blurplestar:857907156099792917> __Eğlence Komutlar__ » \`${prefix}eğlence\` `,`➥ ***Eğlence Komutlarını Gösterir***`)
-.addField(`> <:blurpleintegrasyon:857907154211045387> __Oyun Komutlar__ » \`${prefix}oyunlar\` `,`➥ ***Oyun Komutlarını Gösterir***`)
-.addField(`> <:blurpleplus:857907151573352479> __Eklenti Komutlar__ » \`${prefix}eklenti\` `,`➥ ***Eklenti Komutlarını Gösterir***`)
-.addField(`» <:blurplelink:857907168430391336> Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+.setDescription(`${hypesquad} Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
+.addField(`> ${user} __Genel Komutlar__ » \`${prefix}genel\` `,`➥ ***Genel Komutları Gösterir***`)
+.addField(`> ${guard} __Koruma Komutlar__ » \`${prefix}koruma\` `,`➥ ***Koruma Komutlarını Gösterir***`)
+.addField(`> ${settings} __Sunucu Komutlar__ » \`${prefix}sunucu\` `,`➥ ***Sunucu Komutlarını Gösterir***`)
+.addField(`> ${star} __Eğlence Komutlar__ » \`${prefix}eğlence\` `,`➥ ***Eğlence Komutlarını Gösterir***`)
+.addField(`> ${integrasyon} __Oyun Komutlar__ » \`${prefix}oyunlar\` `,`➥ ***Oyun Komutlarını Gösterir***`)
+.addField(`> ${plus} __Eklenti Komutlar__ » \`${prefix}eklenti\` `,`➥ ***Eklenti Komutlarını Gösterir***`)
+.addField(`» ${link} Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
 .setThumbnail(client.user.avatarURL)
 .setImage(dragon.banner)
 msg.channel.send(bot)
