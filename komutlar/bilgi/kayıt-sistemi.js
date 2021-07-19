f21d76dfbc89c6f7739146e07a3d7da8ff87bb23
Glitch (short-inconclusive-printer) <none>
2021-07-19 07:45:09 +0000

    👛🍘 Checkpoint ./komutlar/bilgi/kayıt-sistemi.js:33042553/1484

---
+++ komutlar/bilgi/kayıt-sistemi.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+const dragon = require("../../ayarlar.json");
+const { hypesquad, list, link } = require('../../emoji.json')
+const prefix = dragon.prefix
+exports.run = async (client, msg, args) => { 
+    
+const bot = new Discord.MessageEmbed()
+.setAuthor(`${client.user.username} Kayıt Menüsü`,client.user.avatarURL())
+.setColor('0x36393E')
+.setDescription(`${hypesquad} Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
+.addField(`> ${list} __Çekiliş Sistemi__ » \`${prefix}çekiliş-sistemi\` `,`➥ ***Gelişmiş Çekiliş Sistemi***`)
+.addField(`> ${list} __Seviye Sistemi__ » \`${prefix}seviye-sistemi\` `,`➥ ***Gelişmiş Seviye Sistemi***`)
+.addField(`> ${list} __Reaksiyon Sistemi__ » \`${prefix}reaksiyon-sistemi\` `,`➥ ***Gelişmiş Seviye Sistemi***`)
+.addField(`» ${link} Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+.setThumbnail(client.user.avatarURL)
+.setImage(dragon.banner)
+     msg.channel.send(bot)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["kayıtsistemi"],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'kayıt-sistemi'
+};
