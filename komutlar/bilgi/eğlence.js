6f3b2bc1c75b19b402c9c494b5ad61923a6a6b17
Glitch (short-inconclusive-printer) <none>
2021-07-19 07:11:35 +0000

    🎙🚆 Checkpoint ./komutlar/bilgi/eğlence.js:33042553/258

--- komutlar/bilgi/eğlence.js
+++ komutlar/bilgi/eğlence.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
+const { hypesquad, star, link } = require('../../emoji.json')
 const dragon = require("../../ayarlar.json");
 const prefix = dragon.prefix
 exports.run = async (client, msg, args) => { 
@@ -7,9 +8,9 @@ exports.run = async (client, msg, args) => {
 const bot = new Discord.MessageEmbed() 
 .setAuthor(`${client.user.username} Eğlence Yardım Menüsü`,client.user.avatarURL())
 .setColor('0x36393E')
-.setDescription(`<:blurplehypesquad:857920353179009044> Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
-.addField(`> <:blurplestar:857907156099792917> __Ara 112__ » \`${prefix}ara112\` `,`➥ ***Ambulans Çağırır (ciddiye almayın)***`)
-.addField(`> <:blurplestar:857907156099792917> __Araba__ » \`${prefix}araba\` `,`➥ ***Araba Gifi Atar***`)
+.setDescription(`${hypesquad} Dragon botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
+.addField(`> ${star} __Ara 112__ » \`${prefix}ara112\` `,`➥ ***Ambulans Çağırır (ciddiye almayın)***`)
+.addField(`> ${star} __Araba__ » \`${prefix}araba\` `,`➥ ***Araba Gifi Atar***`)
 .addField(`> <:blurplestar:857907156099792917> __Atatürk__ » \`${prefix}atatürk\` `,`➥ ***Ulu Önder Mustafa Kemal Atatürkü Rahmet Ve Saygıyla Anıyoruz***`)
 .addField(`> <:blurplestar:857907156099792917> __Ağla__ » \`${prefix}ağla\` `,`➥ ***Botu Ağlatırsınız***`)
 .addField(`> <:blurplestar:857907156099792917> __Aşk Ölçer__ » \`${prefix}aşk-ölçer\` `,`➥ ***Aşk Ölçer***`)
@@ -19,7 +20,7 @@ const bot = new Discord.MessageEmbed()
 .addField(`> <:blurplestar:857907156099792917> __Espri__ » \`${prefix}espri\` `,`➥ ***Bot Rastgele Espri Atar***`)
 .addField(`> <:blurplestar:857907156099792917> __FBI__ » \`${prefix}fbi\` `,`➥ ***FBI Gif Atar***`)
 .addField(`> <:blurplestar:857907156099792917> __Yumruk At__ » \`${prefix}yumruk-at\` `,`➥ ***Yumruk Atarsınız***`)
-.addField(`» <:blurplelink:857907168430391336> Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+.addField(`» ${link} Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=825659553370734632&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/8KmvJrdnDZ) **|** [Web Sitesi](https://discord.com/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
 .setThumbnail(client.user.avatarURL)
 .setImage(dragon.banner)
 msg.channel.send(bot)
