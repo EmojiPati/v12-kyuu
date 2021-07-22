117e2157b00b73fe97f5aa3539b649fff528ab92
Glitch (short-inconclusive-printer) <none>
2021-07-22 12:19:48 +0000

    🕍🐺 Checkpoint ./komutlar/botlist/bot-onayla.js:33042553/440

--- komutlar/botlist/bot-onayla.js
+++ komutlar/botlist/bot-onayla.js
@@ -14,11 +14,10 @@ let westralogkanal = await db.fetch(`botlistlog.${message.guild.id}`, kanal3.id)
  let developer = db.fetch(`developerrol.${message.guild.id}`);
  let botrol = db.fetch(`botrol.${message.guild.id}`)
    if(!message.member.roles.cache.has(westrabotlistyetkilisi)) return message.channel.send(`${attention} Bu komutu kullanabilmen için <@&${westrabotlistyetkilisi}> adlı role sahip olman lazım!`)
-	let botisim = args[0]
-  let sahip = args[1]
+	let botisim = message.guild.members.cache.get(args[0]);
+    let sahip = message.guild.members.cache.get(args[1]);
 	let log = westralogkanal //  Bot Eklendi / Onaylandı / Rededildi Kanalı 
 	if (!botisim) return message.channel.send(`${igne} Botun ID'sini yazmalısın.`).then(x => x.delete({timeout: 3000}))
-  	if (!sahip) return message.channel.send(`${igne} Bot sahibinin ID'sini yazmalısın.`).then(x => x.delete({timeout: 3000}))
   message.delete()
    const westrabumbeyyyy = new Discord.MessageEmbed()
     .setColor(`0x36393E`)
@@ -27,9 +26,7 @@ let westralogkanal = await db.fetch(`botlistlog.${message.guild.id}`, kanal3.id)
     .setDescription(`${yes} <@${sahip}> adlı kişinin <@${botisim}> adlı botu onaylandı. Onaylayan yetkili: ${message.author}`)
 		client.channels.cache.get(log).send(westrabumbeyyyy);
 		message.channel.send(`${yes2} Botu onayladınız.`).then(x => x.delete({timeout: 3000}))
-  var role1 = message.guild.roles.cache.find(role1 => role1.name === developer);
         sahip.roles.add(developer);
-  var role2 = message.guild.roles.cache.find(role2 => role2.name === botrol);
         botisim.roles.add(botrol)
 };
 
