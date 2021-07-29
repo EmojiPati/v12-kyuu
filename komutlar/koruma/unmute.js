10745f36f6d3925321ace4243ad8f3b07738ab4e
Glitch (short-inconclusive-printer) <none>
2021-07-29 14:50:30 +0000

    🐇🐓 Checkpoint ./komutlar/koruma/unmute.js:33042553/58

--- komutlar/koruma/unmute.js
+++ komutlar/koruma/unmute.js
@@ -4,14 +4,14 @@ const db = require('quick.db');
 const ayarlar = require('../../ayarlar.json');
 const embed = new Discord.MessageEmbed()
 const { attention, muteli, igne, yes,  mention, staff, search, saat, compass, plus, no, mutesiz } = require('../../emoji.json')
-
+let prefix = ayarlar.prefix
 exports.run = async(client, message, args) => {
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${attention} **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
 let modlog = db.fetch(`log_${message.guild.id}`)
 let mutelirol = db.fetch(`mutelirol.${message.guild.id}`)
   let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
   let user = message.guild.member(member)
-  if (!user) return message.channel.send(embed.setDescription(`${attention} ${message.author}, Eksik arguman kullandınız, \`.unmute @etiket/ID\``)).then(m => m.delete({ timeout: 7000 }) && message.delete({ timeout: 7000 }))
+  if (!user) return message.channel.send(embed.setDescription(`${attention} ${message.author}, Eksik arguman kullandınız, \`d!unmute @etiket/ID\``)).then(m => m.delete({ timeout: 7000 }) && message.delete({ timeout: 7000 }))
 
   if (user.id === message.author.id) return message.react(no)
   if (user.id === client.user.id) return message.react(no)
